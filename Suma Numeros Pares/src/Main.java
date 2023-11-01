c6c9c92fffdc1682c7cbdc4b6eefac3334774d65
Voltriixz <skyhunter65yt@gmail.com>
2023-11-01 12:31:24 +0100

    Suma Numeros Pares

---
+++ Suma Numeros Pares/src/Main.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+    int suma = 0;
+
+    do {
+        suma += 2;
+        JOptionPane.showMessageDialog(null,suma);
+    } while (suma <1000);
+
+
+    }
+}
